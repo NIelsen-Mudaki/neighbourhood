2589140178b3b32cc148a7dcb15e1b1dab66b91f
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-21 14:25:12 +0300

    add models

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -10,3 +10,16 @@ class Neighborhood(models.Model):
     pub_date = models.DateTimeField(auto_now_add=True)
     Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     country = CountryField(blank_label='(select country)', default='KE')
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    photo = models.ImageField(upload_to = 'profile_pics/', blank=True, default='profile_pics/default.jpg')
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE, blank=True, default='1')
+
+class Posts(models.Model):
+    post = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)    
+    neighborhood = models.ForeignKey(Neighborhood,on_delete=models.CASCADE)
+    Author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
