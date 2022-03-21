5fe23c9b56439959275bba78659d38fb74230fb9
Nielsen-Mudaki <jumbanielsen@gmail.com>
2022-03-21 14:18:44 +0300

    add homepage

--- neighbourhood/models.py
+++ neighbourhood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    occupants_count = models.IntegerField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    Admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    country = CountryField(blank_label='(select country)', default='KE')
